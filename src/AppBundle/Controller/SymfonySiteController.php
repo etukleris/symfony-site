1c3f8fb6c107060bb9b9fd520221b459c3e1b1c0
Edgaras <etukleris@gmail.com>
2019-08-01 15:27:04 +0300

    decided to go with rebuilding website like codeigniter is. This commit adds static pages; will work on database logic next

--- src/AppBundle/Controller/SymfonySiteController.php
+++ src/AppBundle/Controller/SymfonySiteController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class SymfonySiteController extends Controller
 {
     /**
-     * @Route("/testpage", name="symfony-site")
+     * @Route("/index", name="site-index")
      */
     public function indexAction(Request $request)
     {
@@ -38,5 +38,57 @@ class SymfonySiteController extends Controller
     public function detailsAction($id, Request $request)
     {
         return $this->render('SymfonySite/details.html.twig');
+    }
+    
+    //css static pages
+    /**
+     * @Route("/css1", name="css1page")
+     */
+    public function css1pageAction(Request $request)
+    {
+        return $this->render('SymfonySite/csstests/csstest1.html.twig');
+    }
+    /**
+     * @Route("/css2", name="css2page")
+     */
+    public function css2pageAction(Request $request)
+    {
+        return $this->render('SymfonySite/csstests/csstest2.html.twig');
+    }
+    /**
+     * @Route("/css3", name="css3page")
+     */
+    public function css3pageAction(Request $request)
+    {
+        return $this->render('SymfonySite/csstests/csstest3.html.twig');
+    }
+    /**
+     * @Route("/css4", name="css4page")
+     */
+    public function css4pageAction(Request $request)
+    {
+        return $this->render('SymfonySite/csstests/csstest4.html.twig');
+    }
+    /**
+     * @Route("/css5", name="css5page")
+     */
+    public function css5pageAction(Request $request)
+    {
+        return $this->render('SymfonySite/csstests/csstest5.html.twig');
+    }
+    /**
+     * @Route("/css6", name="css6page")
+     */
+    public function css6pageAction(Request $request)
+    {
+        return $this->render('SymfonySite/csstests/csstest6.html.twig');
+    }
+    
+    /**
+     * @Route("/dummy", name="dummypage")
+     */
+    public function dummypageAction(Request $request)
+    {
+        return $this->render('SymfonySite/dummy.html.twig');
     }
 }
